eba6e009292e37a8b862de1439e1e0d99b2fbf01
analauradias <analauramanfron@gmail.com>
2023-11-14 16:55:38 -0300

    terminada

--- script.js
+++ script.js
@@ -1,27 +1,64 @@
-function clq1() {
-    alert("Você clicou no Botão 1");
-}
-
-let bt2 = document.getElementById("bt2");
-bt2.addEventListener("click", clq2);
-
-function clq2() {
-    alert("Você clicou no Botão 2");
-}
 
 let valor1 = document.querySelector("#valor1");
 let valor2 = document.querySelector("#valor2");
 let resultado = document.querySelector("#resultado");
 
-valor1.addEventListener("keyup", function() {
+let btSoma = document.querySelector("#btSoma");
+btSoma.addEventListener("click", function () {
     somar(Number(valor1.value), Number(valor2.value));
 });
 
-valor2.addEventListener("keyup", function() {
-    somar(Number(valor1.value), Number(valor2.value));
+let btSub = document.querySelector("#btSub");
+btSub.addEventListener("click", function () {
+    subtrair(Number(valor1.value), Number(valor2.value));
 });
+
+let btMulti = document.querySelector("#btMulti");
+btMulti.addEventListener("click", function () {
+    multiplicacao(Number(valor1.value), Number(valor2.value));
+});
+
+let btDiv = document.querySelector("#btDiv");
+btDiv.addEventListener("click", function () {
+    divisao(Number(valor1.value), Number(valor2.value));
+});
+
+function divisao(a, b) {
+    let div = a / b;
+    resultado.innerHTML = div;
+}
+
+function multiplicacao(a, b) {
+    let multiplicacao = a * b;
+    resultado.innerHTML = multiplicacao;
+}
+
+function subtrair(a, b) {
+    let subtracao = a - b;
+    resultado.innerHTML = subtracao;
+}
 
 function somar(a, b) {
     let soma = a + b;
     resultado.innerHTML = soma;
 }
+
+let selectedLanguage = document.querySelector("#idioma");
+selectedLanguage.addEventListener("change", function () {
+    if (this.value == "pt-br") {
+        btSoma.value = "Soma";
+        btSub.value = "Subtração";
+        btMulti.value = "Multiplicação";
+        btDiv.value = "Divisão";
+    } else if (this.value == "en") {
+        btSoma.value = "Sum";
+        btSub.value = "subtraction";
+        btMulti.value = "multiplication";
+        btDiv.value = "division";
+    } else if (this.value == "es") {
+        btSoma.value = "Suma";
+        btSub.value = "sustracción";
+        btMulti.value = "multiplicación";
+        btDiv.value = "división";
+    }
+})
